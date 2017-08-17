829339866ebfab24c7070d36980ad8b5cded2e13
Michal Podhradsky <michal.podhradsky@aggiemail.usu.edu>
2017-08-17 11:22:31 -0700

    Compiles with weak symbols

--- apps/rumprun_rust/components/hellorust/rustest/src/main.rs
+++ apps/rumprun_rust/components/hellorust/rustest/src/main.rs
@@ -1,18 +1,26 @@
+#![feature(linkage)]
+
 fn main() {
- println!("Hello rust!");
- unsafe {
- 	camkes_ev_emit();
- }
- println!("Waiting for event!");
- unsafe {
-	 camkes_ev1_wait();
- }
- println!("Got event!");
- 
+    println!("Hello rust!");
+    unsafe {
+        (*((&camkes_ev_emit as *const *const extern "C" fn()) as *const extern "C" fn()))();
+    }
+    println!("Waiting for event!");
+    unsafe {
+        std::mem::transmute::<*const extern "C" fn(), extern "C" fn()>(camkes_ev1_wait)();
+    }
+    println!("Got event!");
+
 }
 
 #[no_mangle]
-extern {
-    fn camkes_ev_emit();
-    fn camkes_ev1_wait(); 
+extern "C" {
+    #[linkage = "extern_weak"]
+    static camkes_ev_emit: *const extern "C" fn();
+    #[linkage = "extern_weak"]
+    static camkes_ev1_wait: *const extern "C" fn();
+
+	#[no_mangle]
+	static camkes_buffer: *mut [u8];
+    
 }
