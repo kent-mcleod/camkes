7a0b9e8eadf049fc995850adbaf055aa5a7ee163
Jim Carciofini <jim.carciofini@adventiumlabs.com>
2020-01-14 11:33:50 -0600

    New implementation of AADL Event Data Ports using wait-free circular queue. - Initially a copy of aadl-event-direct. Added wait-free circular queue. - Three styles of receiver: poll, wait and callback. - Enqueue and dequeue are non-blocking. - Could be generalized to handle AADL event and data ports.

---
+++ apps/aadl-eventdata-direct-new/components/Receiver/src/receiver.c
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * Copyright 2019 Adventium Labs
+ * Modifications made to original
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_Adventium_BSD)
+ */
+
+#include <camkes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <counter.h>
+#include <data.h>
+#include <queue.h>
+
+//------------------------------------------------------------------------------
+// User specified input data receive handler for AADL Input Event Data Port (in) named
+// "p1_in".
+
+void p1_in_aadl_event_data_receive(counter_t numDropped, data_t *data) {
+    printf("%s: received: %d  numDropped: %" PRIcounter "\n", get_instance_name(), data->x, numDropped);
+}
+
+//------------------------------------------------------------------------------
+// Implementation of AADL Input Event Data Port (in) named "p1_in"
+// Three styles: poll, wait and callback.
+//
+// Callback would typically be avoid for safety critical systems. It is harder
+// to analyze since the callback handler is run on some arbitrary thread.
+//
+// NOTE: If we only need polling style receivers, we can get rid of SendEvent
+
+recv_queue_t recvQueue;
+
+// Assumption: only one thread is calling this and/or reading p1_in_recv_counter.
+bool p1_in_aadl_event_data_poll(counter_t *numDropped, data_t *data) {
+    return queue_dequeue(&recvQueue, numDropped, data);
+}
+
+void p1_in_aadl_event_data_wait(counter_t *numDropped, data_t *data) {
+    while (!p1_in_aadl_event_data_poll(numDropped, data)) {
+        p1_in_SendEvent_wait();
+    }
+}
+
+static void p1_in_handler(void *v) {
+    counter_t numDropped;
+    data_t data;
+
+    // Handle ALL events that have been queued up
+    while (p1_in_aadl_event_data_poll(&numDropped, &data)) {
+    	p1_in_aadl_event_data_receive(numDropped, &data);
+    }
+    while (! p1_in_SendEvent_reg_callback(&p1_in_handler, NULL));
+}
+
+//------------------------------------------------------------------------------
+// Testing - Three tests for the different styles: poll, wait and callback.
+//
+// NOTE: The constants in the tests were chosen to cause a variety of
+// situations at runtime including dropped packets and no data
+// available. These numbers may not cause the same variety of behaviour in
+// different test environments.
+
+void run_poll(void) {
+    counter_t numDropped;
+    data_t data;
+
+    recv_queue_init(&recvQueue, p1_in_queue);
+    while (true) {
+
+        // Busy loop to wait a bit and slow things down
+        for(unsigned int j = 0; j < 80000; ++j){
+            seL4_Yield();
+        }
+
+        // Random number of polls for testing
+        int n = (random() % 10);
+        for(unsigned int j = 0; j < n; ++j){
+            bool dataReceived = p1_in_aadl_event_data_poll(&numDropped, &data);
+            if (dataReceived) {
+                p1_in_aadl_event_data_receive(numDropped, &data);
+            } else {
+                printf("%s: received nothing\n", get_instance_name());
+            }
+        }
+
+    }
+
+}
+
+void run_wait(void) {
+    counter_t numDropped;
+    data_t data;
+
+    recv_queue_init(&recvQueue, p1_in_queue);
+    while (true) {
+        p1_in_aadl_event_data_wait(&numDropped, &data);
+        p1_in_aadl_event_data_receive(numDropped, &data);
+
+        // Busy loop to wait a bit and slow things down randomly
+        int n = (random() % 10) * 5000;
+        for(unsigned int j = 0; j < n; ++j){
+            seL4_Yield();
+        }
+
+    }
+}
+
+int run_callback(void) {
+    recv_queue_init(&recvQueue, p1_in_queue);
+    return p1_in_SendEvent_reg_callback(&p1_in_handler, NULL);
+}
+
+int run(void) {
+    // Pick one receive style to test.
+    run_poll();
+    //run_wait();
+    //return run_callback();
+}
+
+// Emacs Declarations
+// Local Variables:
+// mode: c
+// c-basic-offset: 4
+// indent-tabs-mode: nil
+// End:              
---
+++ apps/aadl-eventdata-direct-new/components/Sender/src/sender.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * Copyright 2019 Adventium Labs
+ * Modifications made to original
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_Adventium_BSD)
+ */
+
+#include <camkes.h>
+#include <stdio.h>
+#include <string.h>
+#include <sel4/sel4.h>
+#include <stdlib.h>
+#include <counter.h>
+#include <data.h>
+#include <queue.h>
+
+//------------------------------------------------------------------------------
+// Implementation of AADL Input Event Data Port (out) named "p1_out"
+//
+// NOTE: If we only need polling style receivers, we can get rid of the SendEvent
+
+// Assumption: only one thread is calling this and/or reading p1_in_recv_counter.
+void p1_out_aadl_event_data_send(data_t *data) {
+    queue_enqueue(p1_out_queue, data);
+    p1_out_SendEvent_emit();
+}
+
+//------------------------------------------------------------------------------
+// Testing
+
+int run(void) {
+
+    int i = 0;
+    int err = 0;
+    data_t data;
+
+    queue_init(p1_out_queue);
+
+    while (1) {
+
+        // Busy loop to slow things down
+        for(unsigned int j = 0; j < 100000; ++j){
+            seL4_Yield();
+        }
+
+        // Send a random number of data elements
+        int n = (random() % 10);
+        for(unsigned int j = 0; j < n; ++j){
+            ++i;
+            // Stage data
+            data.x = i;
+            printf("%s: sending: %d\n", get_instance_name(), data.x);
+            // Send the data
+            p1_out_aadl_event_data_send(&data);          
+        }
+    }
+}
+
+// Emacs Declarations
+// Local Variables:
+// mode: c
+// c-basic-offset: 4
+// indent-tabs-mode: nil
+// End:              
+
+
